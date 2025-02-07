db9b478f0600e2d92bbba31e6b8435465999f9f4
unbreakk1 <95477818+unbreakk1@users.noreply.github.com>
2025-02-07 11:35:19 +0100

    add Beutel add method

--- src/main/java/unbreakk1/Beutel.java
+++ src/main/java/unbreakk1/Beutel.java
@@ -8,7 +8,8 @@ public class Beutel
         Object value;
         Node next;
 
-        Node(Object value) {
+        Node(Object value)
+        {
             this.value = value;
             this.next = null;
         }
@@ -25,4 +26,20 @@ public class Beutel
         this.size = 0;
     }
 
+    public void add(Object value)
+    {
+        Node newNode = new Node(value);
+        if (head == null)
+        {
+            // If the list is empty, set both head and tail to the new node
+            head = newNode;
+            tail = newNode;
+        } else
+        {
+            // Otherwise, add the new node to the end of the list and update the tail
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
 }
