5d51ffedbc2c668bf1ed325e23f9f861f27e4744
unbreakk1 <95477818+unbreakk1@users.noreply.github.com>
2025-02-07 10:44:10 +0100

    add Students

--- src/main/java/unbreakk1/Main.java
+++ src/main/java/unbreakk1/Main.java
@@ -1,9 +1,21 @@
 ﻿package unbreakk1;
 
+import java.util.List;
+import java.util.ArrayList;
+
+;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        List<Student> students = new ArrayList<>();
+
+        students.add(new Student("Wanda", "Waffles", "S001"));
+        students.add(new Student("Tommy", "Tickles", "S002"));
+        students.add(new Student("Betty", "Bubbles", "S003"));
+        students.add(new Student("Freddy", "Fizzlepop", "S004"));
+        students.add(new Student("Larry", "Lemonpants", "S005"));
 
     }
 }
